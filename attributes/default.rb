f5613555699e3c79ae1c5b429b292f9f5fd4291a
Dhruv Sahni <dsahni@chef.io>
2015-04-24 15:17:06 -0700

    changed docker version

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-override['docker']['version'] = '1.6.0'
+override['docker']['version'] = '0.9.1'
 override['docker']['storage_driver'] = 'aufs'
 
-default['delivery-truck']['kitchen_driver'] = 'openstack'
+default['delivery-truck']['kitchen_driver'] = 'docker'
