d492fd1aca6d836878e474aeec03a37a7c36347f
Dhruv Sahni <dsahni@chef.io>
2015-04-24 15:52:29 -0700

    changed the driver back to openstack

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
 override['docker']['version'] = '0.9.1'
 override['docker']['storage_driver'] = 'aufs'
 
-default['delivery-truck']['kitchen_driver'] = 'docker'
+default['delivery-truck']['kitchen_driver'] = 'openstack'
